a0d663db94c3bd881968c7a273eef6c331e263ab
Hayden <haydendonald@gmail.com>
2019-10-05 14:55:43 +1000

    Add difficulty unit test

---
+++ BattleShipTests/src/GameControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwinGameSDK;
+
+namespace Tests
+{
+    [TestClass()]
+    public class GameControllerTests
+    {
+        //Check that the difficulty gets set correctly
+        [TestMethod()]
+        public void SetDifficultyTest()
+        {
+            GameController.SetDifficulty(AIOption.Easy);
+            GameController.SetAIPlayer();
+            if (GameController.ComputerPlayer.GetType() != typeof(Nullable))
+            {
+                Assert.Fail("ai player was not set correctly");
+            }
+
+            GameController.SetDifficulty(AIOption.Medium);
+            GameController.SetAIPlayer();
+            if (GameController.ComputerPlayer.GetType() != typeof(AIMediumPlayer))
+            {
+                Assert.Fail("ai player was not set correctly");
+            }
+
+            GameController.SetDifficulty(AIOption.Hard);
+            GameController.SetAIPlayer();
+            if (GameController.ComputerPlayer.GetType() != typeof(AIHardPlayer))
+            {
+                Assert.Fail("ai player was not set correctly");
+            }
+        }
+    }
+}
