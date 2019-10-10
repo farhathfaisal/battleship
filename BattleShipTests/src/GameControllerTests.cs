060398e6924d685ceb55c5b333ce10de4fcf87d3
Farhath Faisal <itsamefarhath@gmail.com>
2019-10-10 15:47:20 +1100

    Add AttackResultGameOver tests

--- BattleShipTests/src/GameControllerTests.cs
+++ BattleShipTests/src/GameControllerTests.cs
@@ -47,6 +47,25 @@ namespace Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+		public void AttackResultGameOverTest ()
+		{
+			AttackResult attackResult = new AttackResult (ResultOfAttack.GameOver, "Game over", 0, 0);
+			string actual = attackResult.ToString ();
+			string expected = "Game over";
+			Assert.AreEqual (expected, actual);
+		}
+
+        [TestMethod()]
+		public void AttackResultGameOverShipTest ()
+		{
+            Ship ship = new Ship (ShipName.AircraftCarrier);
+			AttackResult attackResult = new AttackResult (ResultOfAttack.GameOver, ship, "Game over", 0, 0);
+			string actual = attackResult.ToString ();
+			string expected = "Game over " + ship.Name;
+			Assert.AreEqual (expected, actual);
+		}
+
         [TestMethod]
         public void Shiptest()
         {
